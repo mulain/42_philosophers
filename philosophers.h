8b0e76ca8824ad28fea39d228bf3accf25386fec
Wolf Mardin <wmardin@c2s15d86.42wolfsburg.de>
2022-10-21 18:02:36 +0200

    auto add & push

---
+++ philosophers.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wmardin <wmardin@student.42wolfsburg.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/21 17:59:59 by wmardin           #+#    #+#             */
+/*   Updated: 2022/10/21 18:02:27 by wmardin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+
+# define PHILOSPHERS_H
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <pthread.h>
+
+typedef struct envelope
+{
+
+}	t_envl;
+
+//0_main.c
+//int		main(int argc, char **argv, char **env);
+
+#endif
